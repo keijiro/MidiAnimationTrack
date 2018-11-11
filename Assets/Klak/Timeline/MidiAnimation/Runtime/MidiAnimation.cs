3d1ce6a0a5198b0dc79bdfa2e664d220729c6aef
Keijiro Takahashi <keijiro@gmail.com>
2018-11-11 23:51:09 +0900

    Note animation

--- Assets/Klak/Timeline/MidiAnimation/Runtime/MidiAnimation.cs
+++ Assets/Klak/Timeline/MidiAnimation/Runtime/MidiAnimation.cs
@@ -15,27 +15,78 @@ namespace Klak.Timeline
 
         #endregion
 
-        #region Public properties
+        #region Public properties and methods
 
         public float DurationInSecond {
             get { return duration / tempo * 60 / ticksPerQuarterNote; }
+        }
+
+        public float GetValue(Playable playable, MidiControl control, MidiControlMode mode)
+        {
+            var t = (float)playable.GetTime() % DurationInSecond;
+            if (mode == MidiControlMode.ControlChange)
+                return GetCCValue(t, control.controlNumber);
+            else // MonoNote
+                return GetNoteValue(t, control.noteNumber, control.octave);
+        }
+
+        #endregion
+
+        #region MIDI message operators
+
+        static bool IsCC(ref MidiEvent e, int ccNumber)
+        {
+            return ((e.status & 0xb0) == 0xb0) && e.data1 == ccNumber;
+        }
+
+        static bool IsNote(ref MidiEvent e, int note, int octave)
+        {
+            if ((e.status & 0xe0) != 0x80) return false;
+
+            var num = e.data1;
+
+            // Octave test
+            if (octave >= 0 && num / 12 != octave) return false;
+
+            // Note (interval) test
+            if (note >= 0 && num % 12 != note) return false;
+
+            return true;
+        }
+
+        static bool IsNoteOn(ref MidiEvent e)
+        {
+            return (e.status & 0xf0) == 0x90;
         }
 
         #endregion
 
         #region Private variables and methods
 
-        (int i0, int i1) GetEventIndexAroundTick(uint tick, int controlNumber)
+        (int i0, int i1) GetCCEventIndexAroundTick(uint tick, int controlNumber)
         {
             var last = -1;
             for (var i = 0; i < events.Length; i++)
             {
                 ref var e = ref events[i];
-                if (e.data1 != controlNumber) continue;
+                if (!IsCC(ref e, controlNumber)) continue;
                 if (e.time > tick) return (last, i);
                 last = i;
             }
             return (last, last);
+        }
+
+        int GetNoteEventBeforeTick(uint tick, int note, int octave)
+        {
+            var last = -1;
+            for (var i = 0; i < events.Length; i++)
+            {
+                ref var e = ref events[i];
+                if (e.time > tick) break;
+                if ((e.status & 0xe0u) != 0x80u) continue;
+                if (IsNote(ref e, note, octave)) last = i;
+            }
+            return last;
         }
 
         float ConvertTicksToSecond(uint tick)
@@ -45,14 +96,12 @@ namespace Klak.Timeline
 
         #endregion
 
-        #region PlayableBehaviour overrides
+        #region Value calculation methods
 
-        public float GetCCValue(Playable playable, int controlNumber)
+        public float GetCCValue(float time, int controlNumber)
         {
-            var t = (float)playable.GetTime() % DurationInSecond;
-
-            var tick = (uint)(tempo * t / 60 * ticksPerQuarterNote);
-            var pair = GetEventIndexAroundTick(tick, controlNumber);
+            var tick = (uint)(tempo * time / 60 * ticksPerQuarterNote);
+            var pair = GetCCEventIndexAroundTick(tick, controlNumber);
 
             if (pair.i0 < 0) return 0;
             if (pair.i1 < 0) return events[pair.i0].data2 / 127.0f;
@@ -66,7 +115,16 @@ namespace Klak.Timeline
             var v0 = e0.data2 / 127.0f;
             var v1 = e1.data2 / 127.0f;
 
-            return Mathf.Lerp(v0, v1, Mathf.Clamp01((t - t0) / (t1 - t0)));
+            return Mathf.Lerp(v0, v1, Mathf.Clamp01((time - t0) / (t1 - t0)));
+        }
+
+        public float GetNoteValue(float time, int note, int octave)
+        {
+            var tick = (uint)(tempo * time / 60 * ticksPerQuarterNote);
+            var i = GetNoteEventBeforeTick(tick, note, octave);
+            if (i < 0) return 0;
+            ref var e = ref events[i];
+            return IsNoteOn(ref e) ? e.data2 / 127.0f : 0;
         }
 
         #endregion
--- Assets/Klak/Timeline/MidiAnimation/Runtime/MidiAnimationMixer.cs
+++ Assets/Klak/Timeline/MidiAnimation/Runtime/MidiAnimationMixer.cs
@@ -18,38 +18,40 @@ namespace Klak.Timeline
 
         public override void ProcessFrame(Playable playable, FrameData info, object playerData)
         {
-            var target = playerData as GameObject;
-            if (target == null) return;
-
             var resolver = playable.GetGraph().GetResolver();
 
             foreach (var ctrl in controls)
             {
+                // Target component
+                var component = ctrl.targetComponent.Resolve(resolver);
+                if (component == null) continue;
+
+                // Target property
+                var prop = component.GetType().GetProperty(ctrl.propertyName);
+                if (prop == null) continue;
+
+                // Controller value accumulation
                 var acc = 0.0f;
 
                 for (var i = 0; i < playable.GetInputCount(); i++)
                 {
-                    var clip = (ScriptPlayable<MidiAnimation>)playable.GetInput(i);
-                    var value = clip.GetBehaviour().GetCCValue(clip, ctrl.controlNumber);
-                    acc += playable.GetInputWeight(i) * value;
+                    var clipPlayable = (ScriptPlayable<MidiAnimation>)playable.GetInput(i);
+                    var midiAnim = clipPlayable.GetBehaviour();
+                    var clipValue = midiAnim.GetValue(clipPlayable, ctrl, controlMode);
+                    acc += playable.GetInputWeight(i) * clipValue;
                 }
 
-                var component = ctrl.targetComponent.Resolve(resolver);
-                if (component == null) continue;
+                var vec = Vector4.Lerp(ctrl.vector0, ctrl.vector1, acc);
 
-                var prop = component.GetType().GetProperty(ctrl.propertyName);
-                if (prop == null) continue;
-
-                var v = Vector4.Lerp(ctrl.vector0, ctrl.vector1, acc);
-
+                // Update the target property.
                 if (prop.PropertyType == typeof(float))
-                    prop.SetValue(component, v.x, null);
+                    prop.SetValue(component, vec.x, null);
                 else if (prop.PropertyType == typeof(Vector3))
-                    prop.SetValue(component, (Vector3)v, null);
+                    prop.SetValue(component, (Vector3)vec, null);
                 else if (prop.PropertyType == typeof(Quaternion))
-                    prop.SetValue(component, Quaternion.Euler(v), null);
+                    prop.SetValue(component, Quaternion.Euler(vec), null);
                 else if (prop.PropertyType == typeof(Color))
-                    prop.SetValue(component, (Color)v, null);
+                    prop.SetValue(component, (Color)vec, null);
             }
         }
 

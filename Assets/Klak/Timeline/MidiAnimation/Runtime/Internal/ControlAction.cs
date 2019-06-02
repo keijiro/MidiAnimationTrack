300e0a269d8f13c8de03f7ea756cbf25016d3c6b
Keijiro Takahashi <keijiro@gmail.com>
2019-06-02 21:22:04 +0900

    Refactoring on ControlAction

---
+++ Assets/Klak/Timeline/MidiAnimation/Runtime/Internal/ControlAction.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Klak.Timeline
+{
+    //
+    // ControlAction class that invokes a property setter implemented in a
+    // given target component.
+    //
+    abstract class ControlAction
+    {
+        public static ControlAction
+            CreateAction(object target, string propertyName)
+        {
+            var type = target?.GetType().GetProperty(propertyName)?.PropertyType;
+
+            if (type == typeof(float))
+                return new FloatAction(target, propertyName);
+
+            if (type == typeof(Vector3))
+                return new Vector3Action(target, propertyName);
+
+            if (type == typeof(Quaternion))
+                return new QuaternionAction(target, propertyName);
+
+            if (type == typeof(Color))
+                return new ColorAction(target, propertyName);
+
+            return null;
+        }
+
+        public abstract void Invoke(Vector4 param);
+
+        protected static UnityAction<T>
+            GetPropertySetter<T>(object target, string propertyName)
+        {
+            return (UnityAction<T>)System.Delegate.CreateDelegate
+                (typeof(UnityAction<T>), target, "set_" + propertyName);
+        }
+    }
+
+    class FloatAction : ControlAction
+    {
+        public UnityAction<float> action;
+
+        public FloatAction(object target, string propertyName)
+        {
+            action = GetPropertySetter<float>(target, propertyName);
+        }
+
+        public override void Invoke(Vector4 param) { action(param.x); }
+    }
+
+    class Vector3Action : ControlAction
+    {
+        public UnityAction<Vector3> action;
+
+        public Vector3Action(object target, string propertyName)
+        {
+            action = GetPropertySetter<Vector3>(target, propertyName);
+        }
+
+        public override void Invoke(Vector4 param) { action((Vector3)param); }
+    }
+
+    class QuaternionAction : ControlAction
+    {
+        public UnityAction<Quaternion> action;
+
+        public QuaternionAction(object target, string propertyName)
+        {
+            action = GetPropertySetter<Quaternion>(target, propertyName);
+        }
+
+        public override void Invoke(Vector4 param) { action(Quaternion.Euler(param)); }
+    }
+
+    class ColorAction : ControlAction
+    {
+        public UnityAction<Color> action;
+
+        public ColorAction(object target, string propertyName)
+        {
+            action = GetPropertySetter<Color>(target, propertyName);
+        }
+
+        public override void Invoke(Vector4 param) { action((Color)param); }
+    }
+}
--- Assets/Klak/Timeline/MidiAnimation/Runtime/MidiAnimationMixer.cs
+++ Assets/Klak/Timeline/MidiAnimation/Runtime/MidiAnimationMixer.cs
@@ -1,7 +1,5 @@
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.Playables;
-using System.Reflection;
 
 namespace Klak.Timeline
 {
@@ -15,84 +13,43 @@ namespace Klak.Timeline
 
         #endregion
 
-        #region UnityAction array
-
-        struct ControlAction
-        {
-            public UnityAction<float> floatAction;
-            public UnityAction<Vector3> vector3Action;
-            public UnityAction<Quaternion> quaternionAction;
-            public UnityAction<Color> colorAction;
-        }
+        #region PlayableBehaviour overrides
 
         ControlAction[] _actions;
 
-        #endregion
-
-        #region PlayableBehaviour overrides
-
-        static UnityAction<T> GetPropertyGetter<T>(Component component, string propertyName)
-        {
-            return (UnityAction<T>)System.Delegate.CreateDelegate(
-                typeof(UnityAction<T>), component, "set_" + propertyName);
-        }
-
         public override void OnPlayableCreate(Playable playable)
         {
-            _actions = new ControlAction[controls.Length];
-
             var resolver = playable.GetGraph().GetResolver();
 
-            // Populate UnityEvents for the each controller.
+            // Populate actions for the each controller.
+            _actions = new ControlAction[controls.Length];
+
             for (var i = 0; i < controls.Length; i++)
-            {
-                var ctrl = controls[i];
-
-                // Target component, property name and type.
-                var target = ctrl.targetComponent.Resolve(resolver);
-                var name = ctrl.propertyName;
-                var type = target?.GetType().GetProperty(name)?.PropertyType;
-
-                if (type == typeof(float))
-                    _actions[i].floatAction = GetPropertyGetter<float>(target, name);
-                else if (type == typeof(Vector3))
-                    _actions[i].vector3Action = GetPropertyGetter<Vector3>(target, name);
-                else if (type == typeof(Quaternion))
-                    _actions[i].quaternionAction = GetPropertyGetter<Quaternion>(target, name);
-                else if (type == typeof(Color))
-                    _actions[i].colorAction = GetPropertyGetter<Color>(target, name);
-            }
+                _actions[i] = ControlAction.CreateAction(
+                    controls[i].targetComponent.Resolve(resolver),
+                    controls[i].propertyName
+                );
         }
 
-        public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+        public override void
+            ProcessFrame(Playable playable, FrameData info, object playerData)
         {
             for (var ci = 0; ci < controls.Length; ci++)
             {
                 var ctrl = controls[ci];
-                var act = _actions[ci];
 
                 // Controller value accumulation
                 var acc = 0.0f;
 
                 for (var i = 0; i < playable.GetInputCount(); i++)
                 {
-                    var clipPlayable = (ScriptPlayable<MidiAnimation>)playable.GetInput(i);
-                    var midiAnim = clipPlayable.GetBehaviour();
-                    var clipValue = midiAnim.GetValue(clipPlayable, ctrl, controlMode);
-                    acc += playable.GetInputWeight(i) * clipValue;
+                    var clip = (ScriptPlayable<MidiAnimation>)playable.GetInput(i);
+                    acc += playable.GetInputWeight(i) *
+                        clip.GetBehaviour().GetValue(clip, ctrl, controlMode);
                 }
 
-                var vec = Vector4.Lerp(ctrl.vector0, ctrl.vector1, acc);
-
-                // Controller event invocation
-                if (act.floatAction != null)
-                    act.floatAction(vec.x);
-                else if (act.vector3Action != null)
-                    act.vector3Action((Vector3)vec);
-                else if (act.quaternionAction != null)
-                    act.quaternionAction(Quaternion.Euler(vec));
-                else if (act.colorAction != null)
-                    act.colorAction((Color)vec);
+                // Controller action invocation
+                _actions[ci].Invoke(Vector4.Lerp(ctrl.vector0, ctrl.vector1, acc));
             }
         }
 
